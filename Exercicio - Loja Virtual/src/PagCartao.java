cc05504df9dc49eed4311bf7a01bc2908b4eff3e
SuelyWu <suely9511@gmail.com>
2018-02-20 18:17:33 -0300

    Add files via upload

---
+++ Exercicio - Loja Virtual/src/PagCartao.java
@@ -0,0 +1,2 @@
+public class PagCartao {
+}
---
+++ Exercicio - Loja Virtual/src/Produto.java
@@ -0,0 +1,10 @@
+public class Produto {
+
+    private final String nomeProduto;
+    private final double preco;
+
+    public Produto(final String nomeProduto, final double preco) {
+        this.nomeProduto = nomeProduto;
+        this.preco = preco;
+    }
+}
